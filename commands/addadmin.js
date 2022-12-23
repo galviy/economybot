430c47315b98a06d21aa70bfd82286aeeb04ae3c
Galvin <68190778+galvinlol@users.noreply.github.com>
2022-12-23 16:46:01 +0700

    Update addadmin.js

--- commands/addadmin.js
+++ commands/addadmin.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 module.exports.run = (client, message, args) => {
-	let user = JSON.parse(fs.readFileSync("./database.json"));
-	let owners = JSON.parse(fs.readFileSync("./owners.json"));
-	
-	function getUserFromMention(mention) {
+    let user = JSON.parse(fs.readFileSync("./database.json"));
+    let owners = JSON.parse(fs.readFileSync("./owners.json"));
+
+    function getUserFromMention(mention) {
         if (!mention) return;
         if (mention.startsWith('<@') && mention.endsWith('>')) {
             mention = mention.slice(2, -1);
@@ -14,17 +14,17 @@ module.exports.run = (client, message, args) => {
             return client.users.cache.get(mention);
         }
     }
-	
-	const mention = getUserFromMention(args[0]);
-	console.log(owners)
-	if (owners[message.author.id]) {
-		user[getUserFromMention(args[0]).id].isUserAdministrator = true;
-		fs.writeFile("database.json", JSON.stringify(user), err => {
-			 if (!err) {
-				 message.reply(`Date: <t:${Date.now() / 1000 | 0}:F> Successfully Gave **${mention.username}** **(**${mention.id}**)** ADMIN.`);
-			 }
-			 console.log(err)
-		 })
-	} 
-	
+
+    const mention = getUserFromMention(args[0]);
+    console.log(owners)
+    if (owners[message.author.id]) {
+        user[getUserFromMention(args[0]).id].isUserAdministrator = true;
+        fs.writeFile("database.json", JSON.stringify(user), err => {
+            if (!err) {
+                message.reply(`Date: <t:${Date.now() / 1000 | 0}:F> Successfully Gave **${mention.username}** **(**${mention.id}**)** ADMIN.`);
+            }
+            console.log(err)
+        })
+    }
+
 }
