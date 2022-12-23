4995589a32f03c1c47bcd59893e5021155e1a1a4
Galvin <68190778+galvinlol@users.noreply.github.com>
2022-12-23 16:50:57 +0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -4,13 +4,13 @@ module.exports.run = (client, message, args) => {
     let user = JSON.parse(fs.readFileSync("./database.json"));
     var cooldown = user[message.author.id].dailyclaimed + parseInt(600);
     if (parseInt(cooldown) < Date.now() / 1000 | 0) {
-        user[message.author.id].money += 300000;
+        user[message.author.id].money += 70000;
         user[message.author.id].dailyclaimed = Date.now() / 1000 | 0;
         fs.writeFile("database.json", JSON.stringify(user), err => {
             if (err) {
                 console.log(err)
             } else {
-                return message.reply(`You have claimed daily reward of **5,000** ©️  Lock, now you've **${user[message.author.id].money.toLocaleString()}** ©️`);
+                return message.reply(`You have claimed daily reward of **70,000** ©️  Lock, now you've **${user[message.author.id].money.toLocaleString()}** ©️`);
             }
         })
     } else {
