fdc0a072f9bbd5ca672c59f1777d08379ae2213d
Galvin <68190778+galvinlol@users.noreply.github.com>
2022-12-23 16:47:59 +0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 
 
 
-module.exports.run = (client,message,args) => {
-	let user = JSON.parse(fs.readFileSync("./database.json"));
-	
-	function getUserFromMention(mention) {
+module.exports.run = (client, message, args) => {
+    let user = JSON.parse(fs.readFileSync("./database.json"));
+
+    function getUserFromMention(mention) {
         if (!mention) return;
         if (mention.startsWith('<@') && mention.endsWith('>')) {
             mention = mention.slice(2, -1);
@@ -15,22 +15,22 @@ module.exports.run = (client,message,args) => {
             return client.users.cache.get(mention);
         }
     }
-	if (user[message.author.id].isUserAdministrator) {
-		const mention = getUserFromMention(args[0]);
-		if (!mention) return message.reply("You have to mention the user you want to **BAN**!");
-		if (!user[mention.id]) {
-			console.log(mention)
-			message.reply(`User **${mention.username}** **(**${mention.id})** **does not exist** in Bot's database.`);
-		}
-		if (!args[1]) return message.reply(`Please input the **BAN REASON**!`);
-		user[mention.id].isBanned = true;
-		user[mention.id].whenBanned = Date.now() / 1000 | 0;
-		user[mention.id].bannedReason = args[1];
-		 fs.writeFile("database.json", JSON.stringify(user), err => {
-			 if (!err) {
-				 message.reply(`Date: <t:${Date.now() / 1000 | 0}:F> Successfully banned **${mention.username}** **(**${mention.id}**)** with reason - \`\`${args[1]}\`\``);
-			 }
-		 })
+    if (user[message.author.id].isUserAdministrator) {
+        const mention = getUserFromMention(args[0]);
+        if (!mention) return message.reply("You have to mention the user you want to **BAN**!");
+        if (!user[mention.id]) {
+            console.log(mention)
+            message.reply(`User **${mention.username}** **(**${mention.id})** **does not exist** in Bot's database.`);
+        }
+        if (!args[1]) return message.reply(`Please input the **BAN REASON**!`);
+        user[mention.id].isBanned = true;
+        user[mention.id].whenBanned = Date.now() / 1000 | 0;
+        user[mention.id].bannedReason = args[1];
+        fs.writeFile("database.json", JSON.stringify(user), err => {
+            if (!err) {
+                message.reply(`Date: <t:${Date.now() / 1000 | 0}:F> Successfully banned **${mention.username}** **(**${mention.id}**)** with reason - \`\`${args[1]}\`\``);
+            }
+        })
 
-	}
+    }
 }
