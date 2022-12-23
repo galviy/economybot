4105d7baa051fbe6a84f647f3b30eb96e918fef5
Galvin <68190778+galvinlol@users.noreply.github.com>
2022-12-23 16:46:40 +0700

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const {
+    MessageEmbed
+} = require("discord.js");
 
 module.exports.run = (client, message, args) => {
-	 let user = JSON.parse(fs.readFileSync("./database.json"));
-	 function getUserFromMention(mention) {
+    let user = JSON.parse(fs.readFileSync("./database.json"));
+
+    function getUserFromMention(mention) {
         if (!mention) return;
         if (mention.startsWith('<@') && mention.endsWith('>')) {
             mention = mention.slice(2, -1);
@@ -14,22 +17,26 @@ module.exports.run = (client, message, args) => {
             return client.users.cache.get(mention);
         }
     }
-	const mention = getUserFromMention(args[0]);
-	if (mention && !user[mention.id]) {
-		return message.reply("User **does not** exist in bot database!");
-	}
-	if (mention) {
-		let SuccessEmbed = new Discord.MessageEmbed();
-		SuccessEmbed.setTitle(`💵 ${mention.username}'s information`);
-		SuccessEmbed.setColor("#FF0000")
-		SuccessEmbed.setDescription(`**Wallet**\n${user[mention.id].money.toLocaleString()}\n\n**Deposited Wallet**\n ${user[mention.id].deposited.toLocaleString()}`);
-		message.channel.send({embeds: [SuccessEmbed]}).catch(err=> console.log(err))
-	}
-	if (!mention) {
-		let SuccessEmbeds = new Discord.MessageEmbed();
-		SuccessEmbeds.setTitle(`💵 ${message.author.username}'s Bank`);
-		SuccessEmbeds.setColor("#FF0000")
-		SuccessEmbeds.setDescription(`**Wallet**\n${user[message.author.id].money.toLocaleString()}\n\n**Deposited Wallet**\n ${user[message.author.id].deposited.toLocaleString()}`);
-		message.channel.send({embeds: [SuccessEmbeds]}).catch(err=> console.log(err))
-	}
+    const mention = getUserFromMention(args[0]);
+    if (mention && !user[mention.id]) {
+        return message.reply("User **does not** exist in bot database!");
+    }
+    if (mention) {
+        let SuccessEmbed = new Discord.MessageEmbed();
+        SuccessEmbed.setTitle(`💵 ${mention.username}'s information`);
+        SuccessEmbed.setColor("#FF0000")
+        SuccessEmbed.setDescription(`**Wallet**\n${user[mention.id].money.toLocaleString()}\n\n**Deposited Wallet**\n ${user[mention.id].deposited.toLocaleString()}`);
+        message.channel.send({
+            embeds: [SuccessEmbed]
+        }).catch(err => console.log(err))
+    }
+    if (!mention) {
+        let SuccessEmbeds = new Discord.MessageEmbed();
+        SuccessEmbeds.setTitle(`💵 ${message.author.username}'s Bank`);
+        SuccessEmbeds.setColor("#FF0000")
+        SuccessEmbeds.setDescription(`**Wallet**\n${user[message.author.id].money.toLocaleString()}\n\n**Deposited Wallet**\n ${user[message.author.id].deposited.toLocaleString()}`);
+        message.channel.send({
+            embeds: [SuccessEmbeds]
+        }).catch(err => console.log(err))
+    }
 }
