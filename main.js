bbbc57f7d3ad03292b340ef94135f2f628c24564
Galvins <68190778+galvinlol@users.noreply.github.com>
2022-12-23 16:24:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const Discord = require("discord.js");
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const {
+    Client,
+    Intents
+} = require('discord.js');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
 const prefix = "-";
 
@@ -14,25 +19,25 @@ const timeout = new Set();
 const usertemp = new Object;
 
 for (file of commandFiles) {
-	
-	const commandName = file.split(".")[0];
-	const command = require("./commands/" + commandName);
-	client.commands.set(commandName, command);
+
+    const commandName = file.split(".")[0];
+    const command = require("./commands/" + commandName);
+    client.commands.set(commandName, command);
 }
 
 function createNewUser(discordid) {
     const newuser = discordid = {
-		isUserAdministrator : false,
-		bannedReason: "",
+        isUserAdministrator: false,
+        bannedReason: "",
         whenBanned: 0,
         money: 50000,
         deposited: 0,
-        lastdeposit: 0, 
+        lastdeposit: 0,
         dailyclaimed: 0,
         monthlyclaimed: 0,
         isBanned: false,
     }
-	return newuser;
+    return newuser;
 }
 
 client.on("messageCreate", message => {
@@ -53,7 +58,7 @@ client.on("messageCreate", message => {
                     })
                 return;
             } else {
-				timeout.add(message.author.id);
+                timeout.add(message.author.id);
                 setTimeout(() => {
                     timeout.delete(message.author.id);
                 }, 3000);
@@ -68,24 +73,24 @@ client.on("messageCreate", message => {
                     console.log("created new account -> " + message.author.tag)
                 })
             }
-			if (user[message.author.id].isBanned) {
-				return message.reply(`You've been BANNED from this bot\nDate: <t:${Date.now() / 1000 | 0}:F>  **${message.author.username}** **(**${message.author.id}**)** with reason - \`\`${user[message.author.id].bannedReason}\`\``)
-			}
-			if(!usertemp[message.author.id]) {
-				usertemp[message.author.id] = {
-					playid: false,
-				};
-			}
-			
-			switch (usertemp[message.author.id]) {
-				case true:
-				console.log(usertemp[message.author.id] + "is now playing a game")
-				break;
-				case false:
-				usertemp[message.author.id] = {
-					playid: false,
-				};
-			}
+            if (user[message.author.id].isBanned) {
+                return message.reply(`You've been BANNED from this bot\nDate: <t:${Date.now() / 1000 | 0}:F>  **${message.author.username}** **(**${message.author.id}**)** with reason - \`\`${user[message.author.id].bannedReason}\`\``)
+            }
+            if (!usertemp[message.author.id]) {
+                usertemp[message.author.id] = {
+                    playid: false,
+                };
+            }
+
+            switch (usertemp[message.author.id]) {
+                case true:
+                    console.log(usertemp[message.author.id] + "is now playing a game")
+                    break;
+                case false:
+                    usertemp[message.author.id] = {
+                        playid: false,
+                    };
+            }
 
             command.run(client, message, args, usertemp);
         }
@@ -99,4 +104,4 @@ process.on('unhandledRejection', rejection => {
     console.warn('\n[unhandledRejection]');
     console.warn(rejection);
     console.warn('[/unhandledRejection]\n');
-  });
+});
